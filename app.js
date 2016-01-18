eb9437f977da29556a121657c01d95e1d6dd6d3a
Jeff Cressman <jcressman@nextfaze.com>
2016-01-18 11:14:42 +1030

    Updated examples with Promises

--- app.js
+++ app.js
@@ -1,57 +1,63 @@
 var nano = require('nano')('http://localhost:5984');
+var Q = require('q');
 var credentials = require('./credentials.json');
 var cookies = [];
-var callback = console.log;
 
-nano.auth(credentials.username, credentials.password, function (err, body, headers) {
-  if (err) {
-    return callback(err);
-  }
-
-  if (headers && headers['set-cookie']) {
-    cookies[credentials.username] = headers['set-cookie'];
-    console.log(cookies[credentials.username][0]);
-  }
-
-  callback(null, "Authenticated as " + credentials.username);
-});
-
-// TODO: We need promises here because the cookie hasn't returned yet.
-//       Using the value eventually returned works just fine
-var auth = /AuthSession=(.*?);/.exec(cookies[credentials.username]);
-console.log(auth);
-// var auth = cookies[credentials.username][0];
-nano = require('nano')({
-  url: 'http://localhost:5984',
-  cookie: 'AuthSession=' + auth
-});
-
-// clean up the database we created previously
-nano.db.destroy('alice', function() {
-  // create a new database
-  nano.db.create('alice', function(error, body) {
-    if (!error) {
-      console.log('database alice created!');
-    } else {
-      console.log(error);
+function authenticate(username, password){
+  var deferred = Q.defer();
+  nano.auth(username, password, function (err, body, headers) {
+    if (err) {
+      deferred.reject(console.log(err));
     }
 
-    // specify the database we are going to use
-    var alice = nano.use('alice');
-    // and insert a document in it
-    alice.insert({ crazy: true }, 'rabbit', function(err, body, header) {
-      if (err) {
-        console.log('[alice.insert] ', err.message);
-        return;
+    if (headers && headers['set-cookie']) {
+      cookies[credentials.username] = headers['set-cookie'];
+      deferred.resolve(cookies[credentials.username]);
+    }
+
+    console.log("Authenticated as " + credentials.username);
+  });
+
+  return deferred.promise;
+}
+
+authenticate(credentials.username, credentials.password)
+.then(function(){
+  // var auth = /AuthSession=.*?;/.exec(cookies[credentials.username]);
+  // console.log(auth);
+  nano = require('nano')({
+    url: 'http://localhost:5984',
+    cookie: cookies[credentials.username]
+  });
+})
+.then(function(){
+  // clean up the database we created previously
+  nano.db.destroy('alice', function() {
+    // create a new database
+    nano.db.create('alice', function(error, body) {
+      if (!error) {
+        console.log('database alice created!');
+      } else {
+        console.log(error);
       }
 
-      // change the cookie if couchdb tells us to
-      if (header && header['set-cookie']) {
-        cookies[credentials.username] = header['set-cookie'];
-      }
+      // specify the database we are going to use
+      var alice = nano.use('alice');
+      // and insert a document in it
+      alice.insert({ crazy: true }, 'rabbit', function(err, body, header) {
+        if (err) {
+          console.log('[alice.insert] ', err.message);
+          return;
+        }
 
-      console.log('you have inserted the rabbit.');
-      console.log(body);
+        // change the cookie if couchdb tells us to
+        if (header && header['set-cookie']) {
+          cookies[credentials.username] = header['set-cookie'];
+        }
+
+        console.log('you have inserted the rabbit.');
+        console.log(body);
+      });
     });
   });
 });
